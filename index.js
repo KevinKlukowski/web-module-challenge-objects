39ba16a099031ea7def8a75448ce06d4c4d42fa4
Kevin Klukowski <kklukowski12@gmail.com>
2020-11-05 19:26:23 -0500

    Create a CodeGrade submission

--- index.js
+++ index.js
@@ -128,9 +128,10 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, index) {
+  return `${array[index].name} gave the restaurant a ${array[index].rating} star review, and their feedback was: ${array[index].feedback}`;
 }
+console.log(getReviewByIndex(reviews, 0));
 
 
   
@@ -146,9 +147,10 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length-1].name} gave the restaurant a ${array[array.length -1].rating} star review, and their feedback was: ${array[array.length -1].feedback}`;
 } 
+console.log(getLastReview(reviews));
 
 
 
